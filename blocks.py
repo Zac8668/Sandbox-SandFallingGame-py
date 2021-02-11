39a550d172539dc0d4c23731ec5377afee9c8cf6
Isaac <78173025+Zac8668@users.noreply.github.com>
2021-02-10 23:29:28 -0300

    Bug fixes and main otimization
    Now it uses pygame Pixel Array to check and store information and not one single dictionary.

--- blocks.py
+++ blocks.py
@@ -2,255 +2,294 @@ import pygame
 from random import randint
 
 class sand:
-    
-    def __init__(self, pos, v_border, h_border):
-        self.rect = pygame.Rect(pos[0], pos[1], 10, 10)
-        self.v_border = v_border
-        self.h_border = h_border
+    def __init__(self, pos):
+        self.pos =  [pos[0], pos[1]]
         self.name = 'sand'
         self.density = 2
+        self.SAND = pygame.Color(255,200,0)
+        self.AIR = 0
         
-    def move_down(self):
-        self.rect.y = self.rect.y + 10
+    def move_down(self, pixel_array):
+        try:
+            self.pos[1] += 1
+            pixel_array[self.pos[0], self.pos[1]-1] = self.AIR
+        except IndexError:
+            pass
         
-    def move_down_left(self):
-        self.rect.x -= 10
-        self.rect.y += 10
+    def move_down_left(self, pixel_array):
+        try:
+            self.pos[0] -= 1
+            self.pos[1] += 1
+            pixel_array[self.pos[0]+ 1, self.pos[1]-1] = self.AIR
+        except IndexError:
+            pass
         
-    def move_down_right(self):
-        self.rect.x += 10
-        self.rect.y += 10
+    def move_down_right(self, pixel_array):
+        try:
+            self.pos[0] += 1
+            self.pos[1] += 1
+            pixel_array[self.pos[0]- 1, self.pos[1]-1] = self.AIR
+        except IndexError:
+            pass
         
-    def check_down(self, game_objs, op = 0):
+    def check_down(self, pixel_array):
         down_me = False
-        obj_x = None
-        obj_y = None
-        if len(game_objs) == 1:
-            if game_objs[0].rect.bottom == self.v_border:
-                down_me = True
-        elif len(game_objs) >= 2:
-            for obj in game_objs:
             #move down
-                if obj != self:
-                    if self.rect.bottom == obj.rect.top and self.rect.x == obj.rect.x:
-                        down_me = True
-                        obj_x = obj.rect.x
-                        obj_y = obj.rect.y
-                    elif self.rect.bottom == self.v_border:
-                        down_me = True
-                        obj_x = None
-                        obj_y = None
-                    
-        if op == 1:
-            return [down_me, obj_x, obj_y]
-        else:
-            return down_me
+        try:
+            if pixel_array[self.pos[0], (self.pos[1])+1] != self.AIR:
+                down_me = True
+        except IndexError:
+            down_me = True
+            
+        return down_me
     
-    def check_down_sides(self, game_objs):
-        left_me = False
-        right_me = False
-        
-        check_down = self.check_down(game_objs, 1)
-        
-        if check_down[0] == True and check_down[1] != None and check_down[2] != None:
-            for obj in game_objs:
-                if  obj.rect.x == check_down[1] - 10 and obj.rect.y == check_down[2]:
-                    left_me = True
-                elif check_down[1] == 0:
-                    left_me = True
-                if check_down[1] + 10 == obj.rect.x and obj.rect.y == check_down[2]:
-                    right_me = True
-                elif check_down[1] == self.h_border - 10:
-                    right_me = True
-                        
-                  
-        if left_me == False and right_me == False and check_down[1] != None and check_down[2] != None:
+    def check_down_left(self, pixel_array):
+        down_left_me = False
+            #move down
+        try:
+            if pixel_array[self.pos[0]- 1, (self.pos[1])+ 1] != self.AIR and self.check_down(pixel_array) == True and self.pos[1]+1 <= 30:
+                down_left_me = True
+        except IndexError:
+            down_left_me = True
+            
+        if self.pos[0] <= 0:
+            down_left_me = True
+            
+        return down_left_me
+    
+    def check_down_right(self, pixel_array):
+        down_right_me = False
+            #move down
+        try:
+            if pixel_array[self.pos[0]+ 1, (self.pos[1])+ 1] != self.AIR and self.check_down(pixel_array) == True and self.pos[1]+ 1 <= 30:
+                down_right_me = True
+        except IndexError:
+            down_right_me = True
+            
+        return down_right_me
+                
+    def draw_self(self, pixel_array):
+        try:
+            pixel_array[self.pos[0], self.pos[1]] = self.SAND
+        except IndexError:
+            pass
+    
+    def gravity(self, pixel_array):
+        check_down = self.check_down(pixel_array)
+        check_down_left = self.check_down_left(pixel_array)
+        check_down_right = self.check_down_right(pixel_array)
+
+        if check_down == False:
+            self.move_down(pixel_array)
+            
+        elif check_down_left == False and check_down_right == True:
+            self.move_down_left(pixel_array)
+            
+        elif check_down_left == True and check_down_right == False:
+            self.move_down_right(pixel_array)
+            
+        elif check_down_left == False and check_down_right == False:
             n = randint(1,2)
             if n == 1:
-                return 'r'
+                self.move_down_left(pixel_array)
             if n == 2:
-                return 'l'
-        elif left_me == True and right_me == False:
-            return 'r'
-        elif left_me == False and right_me == True:
-            return 'l'
-                
-    def gravity(self, game_objs):
-        check_down_sides = self.check_down_sides(game_objs)
-        check_down = self.check_down(game_objs)
-        if check_down == False:
-            self.move_down()
-        elif check_down_sides == 'l':
-            self.move_down_left()
-        elif check_down_sides == 'r':
-            self.move_down_right()            
+                self.move_down_right(pixel_array)
+
+        self.draw_self(pixel_array)            
 
 class rock:
-    
-    def __init__(self, pos, v_border, h_border):
-        self.rect = pygame.Rect(pos[0], pos[1], 10, 10)
-        self.v_border = v_border
-        self.h_border = h_border
+    def __init__(self, pos):
+        self.pos = [pos[0], pos[1]]
         self.name = 'rock'
         self.density = 5
+        self.ROCK = pygame.Color(80,80,80)
         
-    def move_down(self):
-        self.rect.y = self.rect.y + 10
+    def move_down(self, pixel_array):
+        try:
+            self.pos[1] += 1
+            pixel_array[self.pos[0], self.pos[1]-1] = pygame.Color(0,0,0)
+        except IndexError:
+            pass
         
-    def check_down(self, game_objs, op = 0):
+    def check_down(self, pixel_array):
         down_me = False
-        obj_x = None
-        obj_y = None
-        if len(game_objs) == 1:
-            if game_objs[0].rect.bottom == self.v_border:
-                down_me = True
-        elif len(game_objs) >= 2:
-            for obj in game_objs:
             #move down
-                if obj != self:
-                    if self.rect.bottom == obj.rect.top and self.rect.x == obj.rect.x:
-                        down_me = True
-                        obj_x = obj.rect.x
-                        obj_y = obj.rect.y
-                    elif self.rect.bottom == self.v_border:
-                        down_me = True
-                        obj_x = None
-                        obj_y = None
-                    
-        if op == 1:
-            return [down_me, obj_x, obj_y]
-        else:
-            return down_me
+        try:
+            if pixel_array[self.pos[0], (self.pos[1])+1] != 0:
+                down_me = True
+        except IndexError:
+            down_me = True
+            
+        return down_me
     
-    def gravity(self, game_objs):
-        check_down = self.check_down(game_objs)
+    def draw_self(self, pixel_array):
+        try:
+            pixel_array[self.pos[0], self.pos[1]] = self.ROCK
+        except IndexError:
+            pass
+    
+    def gravity(self, pixel_array):
+        check_down = self.check_down(pixel_array)
         if check_down == False:
-            self.move_down()           
+            self.move_down(pixel_array)
+        self.draw_self(pixel_array)      
 
 class water:
     
-    def __init__(self, pos, v_border, h_border):
-        self.rect = pygame.Rect(pos[0], pos[1], 10, 10)
-        self.v_border = v_border
-        self.h_border = h_border
+    def __init__(self, pos):
+        self.pos = [pos[0], pos[1]]
         self.name = 'water'
         self.density = 1
+        self.WATER = pygame.Color(10,0,200)
+        self.AIR = 0
         
-    def move_down(self):
-        self.rect.y = self.rect.y + 10
+    def move_down(self, pixel_array):
+        try:
+            self.pos[1] += 1
+            pixel_array[self.pos[0], self.pos[1]-1] = self.AIR
+        except IndexError:
+            pass
         
-    def move_left(self):
-        self.rect.x -= 10
-        self.rect.y += 10
+    def move_left(self, pixel_array):
+        try:
+            self.pos[0] -= 1
+            pixel_array[self.pos[0]+ 1, self.pos[1]] = self.AIR
+        except IndexError:
+            pass
         
-    def move_right(self):
-        self.rect.x += 10
-        self.rect.y += 10
+    def move_right(self, pixel_array):
+        try:
+            self.pos[0] += 1
+            pixel_array[self.pos[0]- 1, self.pos[1]] = self.AIR
+        except IndexError:
+            pass
         
-    def move_rleft(self):
-        self.rect.x -= 10
+    def move_down_left(self, pixel_array):
+        try:
+            self.pos[0] -= 1
+            self.pos[1] += 1
+            pixel_array[self.pos[0]+ 1, self.pos[1]-1] = self.AIR
+        except IndexError:
+            pass
         
-    def move_rright(self):
-        self.rect.x += 10
+    def move_down_right(self, pixel_array):
+        try:
+            self.pos[0] += 1
+            self.pos[1] += 1
+            pixel_array[self.pos[0]- 1, self.pos[1]-1] = self.AIR
+        except IndexError:
+            pass
         
-    def check_down(self, game_objs, op = 0):
+    def check_down(self, pixel_array):
         down_me = False
-        obj_x = None
-        obj_y = None
-        if len(game_objs) == 1:
-            if game_objs[0].rect.bottom == self.v_border:
-                down_me = True
-        elif len(game_objs) >= 2:
-            for obj in game_objs:
             #move down
-                if obj != self:
-                    if self.rect.bottom == obj.rect.top and self.rect.x == obj.rect.x:
-                        down_me = True
-                        obj_x = obj.rect.x
-                        obj_y = obj.rect.y
-                    elif self.rect.bottom == self.v_border:
-                        down_me = True
-                        obj_x = None
-                        obj_y = None
-                    
-        if op == 1:
-            return [down_me, obj_x, obj_y]
-        else:
-            return down_me
+        try:
+            if pixel_array[self.pos[0], (self.pos[1])+1] != self.AIR:
+                down_me = True
+        except IndexError:
+            down_me = True
+            
+        return down_me
     
-    def check_sides(self, game_objs):
-        left_me = False
-        right_me = False
-        
-        check_down = self.check_down(game_objs, 1)
-        
-        if check_down[1] != None and check_down[2] != None:
-            for obj in game_objs:
-                if  obj.rect.x == check_down[1] - 10 and obj.rect.y == check_down[2]:
-                    left_me = True
-                elif check_down[1] == 0:
-                    left_me = True
-                if check_down[1] + 10 == obj.rect.x and obj.rect.y == check_down[2]:
-                    right_me = True
-                elif check_down[1] == self.h_border - 10:
-                    right_me = True
-                                   
-        if left_me == False and right_me == False and check_down[1] != None and check_down[2] != None:
-            n = randint(1,2)
-            if n == 1:
-                return 'r'
-            if n == 2:
-                return 'l'
-        elif left_me == True and right_me == False:
-            return 'r'
-        elif left_me == False and right_me == True:
-            return 'l'
+    def check_down_left(self, pixel_array):
+        down_left_me = False
+            #move down
+        try:
+            if pixel_array[self.pos[0]- 1, (self.pos[1])+ 1] != self.AIR and self.check_down(pixel_array) == True and self.pos[1]+1 <= 30:
+                down_left_me = True
+        except IndexError:
+            down_left_me = True
+            
+        if self.pos[0] <= 0:
+            down_left_me = True
+            
+        return down_left_me
+    
+    def check_down_right(self, pixel_array):
+        down_right_me = False
+            #move down
+        try:
+            if pixel_array[self.pos[0]+ 1, (self.pos[1])+ 1] != self.AIR and self.check_down(pixel_array) == True and self.pos[1]+ 1 <= 30:
+                down_right_me = True
+        except IndexError:
+            down_right_me = True
+            
+        return down_right_me
                 
-    def check_rsides(self, game_objs):
+    def check_left(self, pixel_array):
         left_me = False
-        right_me = False
-        
-        for obj in game_objs:
-            if obj.rect.x == self.rect.x - 10 and obj.rect.y == self.rect.y:
+            #move left
+        try:
+            if pixel_array[self.pos[0]- 1, self.pos[1]] != self.AIR:
                 left_me = True
-            if obj.rect.x == self.rect.x + 10 and obj.rect.y == self.rect.y:
-                right_me = True
-                
-        if left_me == False and self.rect.x == 0:
+        except IndexError:
             left_me = True
-        if right_me == False and self.rect.x == self.h_border - 10:
+            
+        if self.pos[0] <= 0:
+            left_me = True
+            
+        return left_me
+            
+    def check_right(self, pixel_array):
+        right_me = False
+            #move right
+        try:
+            if pixel_array[self.pos[0]+ 1, (self.pos[1])] != self.AIR:
+                right_me = True
+        except IndexError:
             right_me = True
+            
+        return right_me
+              
+    def check_up(self, pixel_array):
+        down_me = False
+            #move up
+        try:
+            if pixel_array[self.pos[0], (self.pos[1])-1] != self.AIR:
+                down_me = True
+        except IndexError:
+            down_me = True
+            
+    def draw_self(self, pixel_array):
+        try:
+            pixel_array[self.pos[0], self.pos[1]] = self.WATER
+        except IndexError:
+            pass
                 
-        if left_me == False and right_me == False:
+    def gravity(self, pixel_array):
+        check_down = self.check_down(pixel_array)
+        check_down_left = self.check_down_left(pixel_array)
+        check_down_right = self.check_down_right(pixel_array)
+        check_left = self.check_left(pixel_array)
+        check_right = self.check_right(pixel_array)
+
+        if check_down == False:
+            self.move_down(pixel_array)
+            
+        elif check_down_left == False and check_down_right == True:
+            self.move_down_left(pixel_array)
+            
+        elif check_down_left == True and check_down_right == False:
+            self.move_down_right(pixel_array)
+            
+        elif check_down_left == False and check_down_right == False:
             n = randint(1,2)
             if n == 1:
-                return 'r'
+                self.move_down_left(pixel_array)
             if n == 2:
-                return 'l'
-        elif left_me == True and right_me == False:
-            return 'r'
-        elif left_me == False and right_me == True:
-            return 'l'
-              
-    def check_up(self, objs):
-        for obj in objs:
-            if obj.rect.x == self.rect.x and obj.rect.y == self.rect.y - 10 and obj.density > self.density:
-                obj.rect.y += 10
-                self.rect.y -= 10
+                self.move_down_right(pixel_array)
                 
-    def gravity(self, game_objs):
-        check_sides = self.check_sides(game_objs)
-        check_down = self.check_down(game_objs)
-        check_rsides = self.check_rsides(game_objs)
-        if check_down == False:
-            self.move_down()
-        elif check_sides == 'l':
-            self.move_left()
-        elif check_sides == 'r':
-            self.move_right()     
-        elif check_rsides == 'l':       
-            self.move_rleft()
-        elif check_rsides == 'r':
-            self.move_rright()
-        elif self.name == 'water':
-            self.check_up(game_objs)
+        elif check_left == False and check_right == True:
+            self.move_left(pixel_array)
+            
+        elif check_left == True and check_right == False:
+            self.move_right(pixel_array)
+            
+        elif check_left == False and check_right == False:
+            n = randint(1,2)
+            if n == 1:
+                self.move_left(pixel_array)
+            if n == 2:
+                self.move_right(pixel_array)
+        
+        self.draw_self(pixel_array) 
--- main.py
+++ main.py
@@ -13,7 +13,14 @@ class sandbox_py:
         self.cur_block = cur_block
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((screen_res[0], screen_res[1]))
+        self.pixelar_sur = pygame.Surface((screen_res[0] / 10, screen_res[1] / 10))
+        self.pixel_array = pygame.PixelArray(self.pixelar_sur)
         self.cur_block_img = pygame.Rect(screen_res[0] - 30, 10, 20, 20)
+        self.game_objs = []
+        
+        self.SAND = pygame.Color(255,200,0)
+        self.ROCK = pygame.Color(80,80,80)
+        self.AIR = 0
         
         pygame.init()
         pygame.display.set_caption('sandbox.py')
@@ -22,7 +29,7 @@ class sandbox_py:
         
     def draw_cur_block(self):
         if self.cur_block == 'sand':
-            pygame.draw.rect(self.screen, (255,200,0), self.cur_block_img)
+            pygame.draw.rect(self.screen, self.SAND, self.cur_block_img)
 
         elif self.cur_block == 'rock':
             pygame.draw.rect(self.screen, (80,80,80), self.cur_block_img)
@@ -38,58 +45,58 @@ class sandbox_py:
         text_rect.topleft = tuple(pos) 
         self.screen.blit(text_img, text_rect)
             
-    def draw_blocks(self, game_objs):
-        objs = 0
-        for obj in game_objs:
-            objs += 1
-            if obj.name == 'sand':
-                pygame.draw.rect(self.screen, (255,200,0), obj.rect)
-                obj.gravity(game_objs)
-            elif obj.name == 'rock':
-                pygame.draw.rect(self.screen, (80,80,80), obj.rect)
-                obj.gravity(game_objs)
-            elif obj.name == 'water':
-                pygame.draw.rect(self.screen, (0,0,200), obj.rect)
-                obj.gravity(game_objs)
-        self.draw_text(str(objs), (60,10))
+    def draw_blocks(self):
+        pygame.transform.scale(self.pixelar_sur, (self.screen_res[0], self.screen_res[1]), self.screen)
         
     def draw_all(self):
-        self.screen.fill((50,50,50))
-        self.draw_blocks(self.all_game_objs)
+        self.draw_blocks()
         self.draw_cur_block()
         self.draw_text(str(self.clock.get_fps())[:2], (10,10))
+        
+    def mouse_to_grid(self, event_pos:list):
+        x_pos = event_pos[0] 
+        y_pos = event_pos[1]
+        
+        x_pos = float(x_pos) / 10
+        x_pos = int(x_pos)
+        x_pos *= 10
+        
+        y_pos = float(y_pos) / 10
+        y_pos = int(y_pos)
+        y_pos *= 10
+        
+        y_pos = int(y_pos/10)
+        x_pos = int(x_pos/10)
+        res = [x_pos, y_pos]
+        return res
 
     def main_loop(self):
         while self.run_game:
             for event in pygame.event.get():
-                #quit properly
+            #quit properly
                 if event.type == pygame.QUIT:
                     pygame.quit()   
                     sys.exit()
+            #gets mouse coordinates and create block on it
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    x_pos = event.pos[0] 
-                    y_pos = event.pos[1]
-                    
-                    x_pos = float(x_pos) / 10
-                    x_pos = int(x_pos)
-                    x_pos *= 10
-                    
-                    y_pos = float(y_pos) / 10
-                    y_pos = int(y_pos)
-                    y_pos *= 10
+                    pos = self.mouse_to_grid(event.pos)
                     
                     if self.cur_block == 'sand':
-                        new_sand_obj = sand([x_pos,y_pos], self.screen_res[1], self.screen_res[0])
-                        self.all_game_objs.append(new_sand_obj)
+                        if self.pixel_array[pos[0], pos[1]] == self.AIR:
+                            new_sand_obj = sand(pos)
+                            self.game_objs.append(new_sand_obj)
                         
                     elif self.cur_block == 'rock':
-                        new_rock_obj = rock([x_pos,y_pos], self.screen_res[1], self.screen_res[0])
-                        self.all_game_objs.append(new_rock_obj)
+                        if self.pixel_array[pos[0], pos[1]] == self.AIR:
+                            new_rock_obj = rock(pos)
+                            self.game_objs.append(new_rock_obj)
                         
                     elif self.cur_block == 'water':
-                        new_water_obj = water([x_pos,y_pos], self.screen_res[1], self.screen_res[0])
-                        self.all_game_objs.append(new_water_obj)
-                        
+                        if self.pixel_array[pos[0], pos[1]] == self.AIR:
+                            new_water_obj = water(pos)
+                            self.game_objs.append(new_water_obj)
+                    
+            #change current block    
                 elif event.type == pygame.KEYDOWN and event.key == pygame.K_q:
                     self.cur_block = 'sand'
                     
@@ -98,8 +105,14 @@ class sandbox_py:
                     
                 elif event.type == pygame.KEYDOWN and event.key == pygame.K_e:
                     self.cur_block = 'rock'
+        #draw, flip and limit fps of the screen
+            objs = 0
+            for obj in self.game_objs:
+                obj.gravity(self.pixel_array)
+                objs += 1
         
             self.draw_all()
+            self.draw_text(str(objs), [60, 10])
             pygame.display.flip()
             self.clock.tick(self.fps)
                         
